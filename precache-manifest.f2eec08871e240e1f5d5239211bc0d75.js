f9845c379a53a129358257d3ef8fcb6f074cf4f1
Ashin <ahmedashin999@gmail.com>
2020-07-08 21:35:08 +0600

    Updates

---
+++ precache-manifest.f2eec08871e240e1f5d5239211bc0d75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50080711934223b50fe369a8c35dbf8e",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "93396f851a00a202f822",
+    "url": "/contactManager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9eb5f7458b704ce8472f",
+    "url": "/contactManager/static/css/main.3c07eb55.chunk.css"
+  },
+  {
+    "revision": "93396f851a00a202f822",
+    "url": "/contactManager/static/js/2.3b9c9e35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactManager/static/js/2.3b9c9e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb5f7458b704ce8472f",
+    "url": "/contactManager/static/js/main.9dee7426.chunk.js"
+  },
+  {
+    "revision": "831df4f3663bc56eca6a",
+    "url": "/contactManager/static/js/runtime-main.f97a6221.js"
+  }
+]);
